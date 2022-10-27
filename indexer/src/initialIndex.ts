127354643138479f20c4309ee206d13359569dc9
visdauas <visdauas@gmail.com>
2022-10-27 20:35:16 +0000

    Make initialIndex partial and presistent

--- indexer/src/initialIndex.ts
+++ indexer/src/initialIndex.ts
@@ -2,61 +2,66 @@ import { getAssetIds } from './assets/assetId';
 import { Asset, getAsset } from './assets/assets';
 import { indexData } from './database/indexData';
 import { initalizeDatabase } from './database/initalize';
-import { dropCollection, loadCollection } from './database/manage';
+import { dropCollection, loadCollection, checkCollectionExsists, getCollectionStatistics } from './database/manageCollection';
 import { getClient } from './database/client';
+import { insertData } from './database/data';
+import { AssetDBEntry } from './types';
 
 export async function initialIndex() {
-  await dropCollection('algoseas_pirates');
-  await initalizeDatabase();
+  const exsists = await checkCollectionExsists('algoseas_pirates');
+  if(!exsists){
+      await initalizeDatabase();
+  }
 
-  const assets: Asset[] = [];
+  await loadCollection('algoseas_pirates');
+
+  const indexedAssetIds = await getClient().dataManager.query({
+    collection_name: 'algoseas_pirates',
+    expr: "assetId != -1",
+    output_fields: ["assetId"],
+  });
+  const indexedAssetIdsArray = indexedAssetIds.data.map((asset: any) => asset.assetId);
+  //await dropCollection('algoseas_pirates');
 
   const assetIds: number[] = await getAssetIds();
 
+  const filteredAssetIds = assetIds.filter((id) => {
+    if (indexedAssetIdsArray.includes(id)) {
+      return false;
+    }
+    return id
+  });
+
   const errorIds: number[] = [];
 
-  for (let i = 0; i < assetIds.length; i++) {
+  for (let i = 0; i < filteredAssetIds.length; i++) {
     try {
-      const asset: Asset = await getAsset(assetIds[i]);
-      assets.push(asset);
-      console.log(`Loaded asset ${i} of ${assetIds.length}`);
+      const asset: Asset | undefined = (await getAsset(filteredAssetIds[i]));
+      if (asset == undefined){
+        errorIds.push(filteredAssetIds[i]);
+      }
+      else{
+        const assetEntry: AssetDBEntry = {
+          assetId: asset.assetId,
+          statVector: [asset.combat, asset.constitution, asset.luck, asset.plunder],
+          combat: asset.combat,
+          constitution: asset.constitution,
+          luck: asset.luck,
+          plunder: asset.plunder,
+          forSale: asset.forSale,
+          lastPrice: asset.lastPrice,
+          lastSoldRound: asset.lastSoldRound,
+        };
+        console.log(assetEntry);
+        insertData('algoseas_pirates', [assetEntry]);
+        console.log(`Loaded asset ${i} of ${filteredAssetIds.length}`);  
+      }
     } catch (error) {
-      console.error(`Error loading asset ${assetIds[i]} of ${assetIds.length}`);
-      errorIds.push(assetIds[i]);
+      console.error(`Error loading asset ${filteredAssetIds[i]} of ${filteredAssetIds.length}`);
+      errorIds.push(filteredAssetIds[i]);
     }
   }
 
-  const milvusClient = getClient();
-
-  let data = [];
-
-  for (let i = 0; i < assets.length; i++) {
-    const stats = [
-      assets[i].combat,
-      assets[i].constitution,
-      assets[i].luck,
-      assets[i].plunder,
-    ];
-
-    data.push({
-      assetId: assets[i].assetId,
-      statVector: stats,
-      combat: stats[0],
-      constitution: stats[1],
-      luck: stats[2],
-      plunder: stats[3],
-      forSale: assets[i].forSale,
-      lastPrice: assets[i].lastPrice,
-      lastSoldRound: assets[i].lastSoldRound,
-    });
-  }
-
-  const mr = await milvusClient.dataManager.insert({
-    collection_name: 'algoseas_pirates',
-    fields_data: data,
-  });
-
-  await loadCollection('algoseas_pirates');
   await indexData();
 
   console.log('done');
