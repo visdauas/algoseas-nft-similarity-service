85b50f7305418eb83fb8191edd5eb32d6b54922f
tothzalan <toth.zalan03@gmail.com>
2022-10-28 23:08:41 +0200

    Moved Asset type to types.ts

--- indexer/src/types.ts
+++ indexer/src/types.ts
@@ -9,3 +9,14 @@ export type AssetDBEntry = {
   lastPrice: number;
   lastSoldRound: number;
 };
+
+export type Asset = {
+  assetId: number,
+  combat: number,
+  constitution: number,
+  luck: number,
+  plunder: number,
+  forSale: boolean,
+  lastPrice: number,
+  lastSoldRound: number,
+}
