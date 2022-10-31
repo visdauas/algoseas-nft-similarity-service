c8f622e80ea42989403ec576d904e7842c0cd445
tothzalan <toth.zalan03@gmail.com>
2022-10-31 17:17:24 +0100

    Indexing sales

--- indexer/src/assets/indexSales.ts
+++ indexer/src/assets/indexSales.ts
@@ -3,36 +3,45 @@ import { AssetSalesDBEntry } from '../types';
 
 let succ = 0;
 let err  = 0;
+let errIds: number[] = [];
 
-async function getEveryTransactionFromAsset(assetId: number) : Promise<any> {
+async function getEveryTransactionFromAsset(assetId: number, txnID: string) : Promise<any> {
   const URL = `${process.env.ALGOINDEXER_URL}/assets/${assetId}/transactions`;
   let transactions = [];
   while(transactions.length == 0) {
     try{
       const response = await fetch(URL);
-      transactions = await response.json();
+      const data = await response.json();
+      console.log(data);
+      if(data.transactions != undefined)
+        transactions = data;
     } catch (_) {
-      setTimeout(() => {}, 5000);
+      setTimeout(() => {}, 1000);
     }
   }
 
-  const trx: any[] = transactions;
+  let trx: any[] = transactions.transactions;
+
+  for(const t of trx) {
+    if(t.id == txnID) return trx;
+  }
 
   while(transactions['next-id'] != undefined) {
     const URL = `${process.env.ALGOINDEXER_URL}/assets/${assetId}/transactions?next=${transactions['next-id']}`;
-    const newResponse = await fetch(URL);
-    const newTransactions = await newResponse.json();
-    trx.concat(...newTransactions);
+    const newResponse: any =await fetch(URL);
+    transactions = await newResponse.json();
+    trx = trx.concat(...transactions.transactions);
   }
+
   return trx;
 }
 
 async function getTransactions(assetId: number, price: number, txnID: string) : Promise<AssetSalesDBEntry> {
-  const transactions = await getEveryTransactionFromAsset(assetId);
+  const transactions = await getEveryTransactionFromAsset(assetId, txnID);
   let round = 0;
   let flag = false;
   let note = '';
-  for(const txn of transactions.transactions) {
+  for(const txn of transactions) {
     if(txn.id == txnID) {
       round = txn['first-valid'];
       flag = true;
@@ -53,18 +62,25 @@ async function getTransactions(assetId: number, price: number, txnID: string) : 
       data.properties.plunder
     ];
   }
-  if(statVector[0] == 0 || statVector[1] == 0 ||
-     statVector[2] == 0 || statVector[3] == 0 ) err++;
-  if(statVector[0] == undefined || statVector[1] == undefined ||
-     statVector[2] == undefined || statVector[3] == undefined ) err++;
-  
 
+  if(statVector[0] == 0 || statVector[1] == 0 ||
+     statVector[2] == 0 || statVector[3] == 0 ) {
+    errIds.push(assetId);
+    err++;
+  }
+  else if(statVector[0] == undefined || statVector[1] == undefined ||
+     statVector[2] == undefined || statVector[3] == undefined ) {
+    errIds.push(assetId);
+    err++;
+  }
   else succ++;
+
   const sales: AssetSalesDBEntry = {
     assetId,
     statVector,
     price,
-    round
+    round,
+    txId: txnID
   };
   return sales;
 }
@@ -75,6 +91,7 @@ export async function getSold(limit = 10) : Promise<AssetSalesDBEntry[]> {
     URL += `&limit=${limit}`;
   const response = await fetch(URL);
   const sold: any = await response.json();
+  console.log("HERE");
   const sales: AssetSalesDBEntry[] = [];
   for(const soldData of sold) {
     const assetId = soldData.assetInformation.SK;
@@ -82,5 +99,6 @@ export async function getSold(limit = 10) : Promise<AssetSalesDBEntry[]> {
     sales.push(await getTransactions(assetId, price, soldData.marketActivity.txnID));
     console.log(`${succ} / ${err}`)
   }
+  console.log(errIds);
   return sales;
 }
